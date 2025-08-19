848febfbe40be98779286fd2a34ef4d6c7315487
DongWoonKim <01030985843z@naver.com>
2025-08-20 00:02:46 +0900

    feat(auth): 로그인 기능 구현 엔드포인트 완성

--- src/main/java/com/trevari/spring/trauthservice/interfaces/http/AuthController.java
+++ src/main/java/com/trevari/spring/trauthservice/interfaces/http/AuthController.java
@@ -1,9 +1,25 @@
 package com.trevari.spring.trauthservice.interfaces.http;
 
+import com.trevari.spring.trauthservice.application.AuthService;
+import com.trevari.spring.trauthservice.interfaces.dto.UserLoginRequestDTO;
+import com.trevari.spring.trauthservice.interfaces.dto.UserLoginResponseDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/api/auth")
 public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/sessions")
+    public UserLoginResponseDTO login(@RequestBody UserLoginRequestDTO req) {
+        return authService.login(req);
+    }
+
 }
