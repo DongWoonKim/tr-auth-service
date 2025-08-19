4b9a5520dc920e5d7869337ed48a2cac657d9ba1
DongWoonKim <01030985843z@naver.com>
2025-08-19 16:15:35 +0900

    tr-auth-service-setup

---
+++ src/main/java/com/trevari/spring/trauthservice/TrAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.trevari.spring.trauthservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrAuthServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrAuthServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/trevari/spring/trauthservice/TrAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trevari.spring.trauthservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrAuthServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
